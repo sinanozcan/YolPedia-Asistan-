5f1bd3976744721aa6af6dd40dee539697b0da21
sinanozcan <s.ozcan24@gmail.com>
2025-12-26 20:46:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1037,7 +1037,7 @@ def main():
         
         /* Ana arkaplan */
         .stApp, .main {
-            background-color: #111111 !important;
+            background-color: #333333 !important;
             color: #e6e6e6 !important;
         }
         
