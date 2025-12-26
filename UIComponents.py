4d3495aa43952527341183e4a405541e8c2614fd
sinanozcan <s.ozcan24@gmail.com>
2025-12-26 18:03:46 +0100

    Update UIComponents.py

--- UIComponents.py
+++ UIComponents.py
@@ -1,11 +1,11 @@
-# UIComponents.py adında yeni bir dosya oluşturun
+# UIComponents.py
 
 import streamlit as st
 
 class UIComponents:
     @staticmethod
     def render_message(message):
-        """Mesajı Streamlit'te görüntüler"""
+        """Mesaji Streamlit'te görüntüler"""
         if isinstance(message, dict) and "role" in message:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
